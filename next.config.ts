81535be418f65c670094717d6585bcb0f35b0740
Dmytro <diman12345677@gmail.com>
2025-01-27 13:58:06 +0200

    feat: upd next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+const withPWA = require("next-pwa")({
+  dest: "public", // Output directory for service worker
+  register: true, // Automatically register service worker
+  skipWaiting: true, // Activate service worker immediately
+  scope: "/app",
+  sw: "service-worker.js",
+  disable: process.env.NODE_ENV === "development", // Disable in development mode
+});
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = withPWA({
+  reactStrictMode: true,
+});
